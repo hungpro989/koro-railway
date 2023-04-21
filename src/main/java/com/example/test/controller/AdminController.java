bb698730cc4a528fb1d1d8d3792b6dca812d99fc
hungpro989 <hungpro989.hp95@gmail.com>
2023-04-21 15:04:50 +0700

    Initial commit

---
+++ src/main/java/com/example/test/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.example.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class AdminController {
+    @GetMapping()
+    public String getString(){
+        return "check ok";
+    }
+}
