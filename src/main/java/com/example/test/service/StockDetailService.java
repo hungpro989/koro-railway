ed7671e9a8765e6807989d97e4abea4a82858af3
hungpro989 <hungpro989.hp95@gmail.com>
2023-04-24 13:32:04 +0700

    update: cac method trong StockDetailService

--- src/main/java/com/example/test/service/StockDetailService.java
+++ src/main/java/com/example/test/service/StockDetailService.java
@@ -2,28 +2,56 @@ package com.example.test.service;
 
 import com.example.test.dto.StockDetailDTO;
 import com.example.test.models.StockDetail;
+import com.example.test.repository.StockDetailRepository;
 import com.example.test.serviceImpl.IStockDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StockDetailService implements IStockDetailService {
+    @Autowired
+    StockDetailRepository stockDetailRepository;
     @Override
     public List<StockDetailDTO> getAll() {
-        return null;
+        List<StockDetailDTO> listDto = new ArrayList<>();
+        List<StockDetail> list = stockDetailRepository.findAll();
+        for(StockDetail var: list){
+            listDto.add((new StockDetailDTO(var)));
+        }
+        return listDto;
+
     }
 
     @Override
     public StockDetailDTO getById(Integer id) {
+        StockDetailDTO dto = new StockDetailDTO();
+        StockDetail stockDetail = stockDetailRepository.findById(id).orElse(null);
+        if(stockDetail!=null){
+            dto = new StockDetailDTO(stockDetail);
+            return dto;
+        }
         return null;
     }
 
     @Override
     public boolean deleteById(Integer id) {
-        return false;
+        try{
+            stockDetailRepository.deleteById(id);
+            return true;
+        }catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public boolean save(StockDetail stockDetail) {
-        return false;
+        try{
+            stockDetailRepository.save(stockDetail);
+            return  true;
+        }catch (Exception e) {
+            return false;
+        }
+
     }
 }
