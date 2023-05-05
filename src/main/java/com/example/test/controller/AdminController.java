d607adf40848c73bc239f162bea283de9dbc8e16
hungpro989 <hungpro989.hp95@gmail.com>
2023-05-05 18:32:49 +0700

    doi url trang chu "/"

--- src/main/java/com/example/test/controller/AdminController.java
+++ src/main/java/com/example/test/controller/AdminController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/")
 public class AdminController {
     @GetMapping
     public ResponseEntity<ResponseObject> homeServer(){
